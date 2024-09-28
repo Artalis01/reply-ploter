5cde3c2a26e24eb19373214bf0d98771d9457034
Artalis01 <74090669+Artalis01@users.noreply.github.com>
2024-09-28 07:00:50 +0000

    disabled uc mode due to unresolved error

--- xcrawler.py
+++ xcrawler.py
@@ -258,17 +258,15 @@ def xcrawl(url, pbar=None, check_login_status=False):
     tweet_replies = []
     topic = []
 
-    settings_path = "settings.py"
-
-    with SB(headless=True, uc=True, user_data_dir='resources/xcrawler/user/profile1', settings_file=settings_path) as sb:
+    with SB(headless=True, user_data_dir='resources/xcrawler/user/profile1') as sb:
         if check_login_status:
             sb.open("https://x.com/")
             login_status = login(sb)
             if login_status:
                 st.info("Login Sukses!")
         else:
-            sb.uc_open_with_reconnect(url, 2)
-            pbar.progress(int((2/11)*100), text='Sedang mempersiapkan data')
+            sb.open(url)
+            pbar.progress(int((2/11)*100), text='(2/11) Sedang mempersiapkan data')
             login_status = login(sb, check_status=True)
 
             if not login_status:
